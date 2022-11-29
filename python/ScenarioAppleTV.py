3c4a525d2450eb4d44759e0ffe3bc389e254362a
estagio2 <estagio2@scenario.ind.br>
2022-11-29 18:07:51 -0300

    base classes update

--- python/ScenarioAppleTV.py
+++ python/ScenarioAppleTV.py
@@ -1,25 +1,22 @@
 import asyncio
-
 from abc import ABC
 
 from pyatv import connect, scan, const
-
 from java.lang import System
+from pyatv.interface import PowerListener, PushListener, Playing, DeviceListener
 
-from pyatv.interface import PushListener, Playing, DeviceListener, PowerListener
 
-
-class ScenarioAppleTV(DeviceListener, PushListener, PowerListener, ABC):
-
+class ScenarioAppleTV:
     def __init__(self, host):
         self._host = host
         self._atv = None
         self._client = None
         self._loop = asyncio.new_event_loop()
-        self._java_listener = None
+        self._listener = AppleTVListeners()
 
     async def scan_network(self):
         atv = await scan(self._loop, hosts=[self._host], timeout=5)
+        await asyncio.sleep(3)
 
         if not atv:
             System.out.println("No devices found...")
@@ -29,74 +26,28 @@ class ScenarioAppleTV(DeviceListener, PushListener, PowerListener, ABC):
             System.out.println(f"Name: {a.name}, Address: {a.address}")
 
         self._atv = atv[0]
+
         return True
 
     async def connect(self):
+        await self.scan_network()
         self._client = await connect(self._atv, self._loop)
-        System.out.println("connected apple tv")
-        self._client.power.listener = self
-        self._client.push_updater.listener = self
+        self._client.power.listener = self._listener
+        self._client.listener = self._listener
+        self._client.push_updater.listener = self._listener
         self._client.push_updater.start()
-
-    def playstatus_update(self, updater, playstatus: Playing) -> None:
-        System.out.println("-")
-        System.out.println("-")
-        System.out.println(str(playstatus))
-        self._java_listener.notify({"playstatus": playstatus})
-
-    def playstatus_error(self, updater, exception: Exception) -> None:
-        System.out.println(str(exception))
-
-    def connection_lost(self, exception: Exception) -> None:
-        System.out.println(str(exception))
-        message = "connection suddenly lost"
-        self._java_listener.notify({"problem": message})
-        System.out.println("Apple TV lost connection")
-
-    def connection_closed(self) -> None:
-        message = "Apple TV closed connection"
-        self._java_listener.notify({"problem": message})
-        System.out.println("Apple TV closed connection")
-
-    def powerstate_update(self, old_state: const.PowerState, new_state: const.PowerState):
-        System.out.println(str(old_state))
-        System.out.println(str(new_state))
-
-    async def channel_down(self):
-        await self._client.remote_control.channel_down()
-
-    async def channel_up(self):
-        await self._client.remote_control.channel_up()
-
-    async def down(self):
-        await self._client.remote_control.down()
-
-    async def home(self):
-        await self._client.remote_control.home()
-
-    async def home_hold(self):
-        await self._client.remote_control.home_hold()
-
-    async def left(self):
-        await self._client.remote_control.left()
-
-    async def menu(self):
-        await self._client.remote_control.menu()
-
-    async def next(self):
-        await self._client.remote_control.next()
-
-    async def pause(self):
-        await self._client.remote_control.pause()
-
-    async def play(self):
-        await self._client.remote_control.play()
 
     async def play_pause(self):
         await self._client.remote_control.play_pause()
 
-    async def previous(self):
-        await self._client.remote_control.previous()
+    async def up(self):
+        await self._client.remote_control.up()
+
+    async def down(self):
+        await self._client.remote_control.down()
+
+    async def left(self):
+        await self._client.remote_control.left()
 
     async def right(self):
         await self._client.remote_control.right()
@@ -104,40 +55,67 @@ class ScenarioAppleTV(DeviceListener, PushListener, PowerListener, ABC):
     async def select(self):
         await self._client.remote_control.select()
 
-    async def set_position(self):
-        await self._client.remote_control.set_position()
+    async def menu(self):
+        await self._client.remote_control.menu()
 
-    async def set_repeat(self):
-        await self._client.remote_control.set_repeat()
+    async def set_position(self, position):
+        await self._client.remote_control.set_position(position)
 
-    async def set_shuffle(self):
-        await self._client.remote_control.select()
+    async def close(self):
+        result = await self._client.close()
+        return result
 
-    async def skip_backward(self):
-        await self._client.remote_control.skip_backward()
+    async def check_if_alive(self):
+        while True:
+            await asyncio.sleep(2)
+            try:
+                is_playing = await asyncio.wait_for(self._client.metadata.playing(), 10)
+            except asyncio.TimeoutError:
+                System.out.println("Nada...")
+                # self._client.close()
+                self._listener.connection_lost(asyncio.TimeoutError)
+            else:
+                System.out.println(" ")
+                System.out.println(str(is_playing))
 
-    async def skip_forward(self):
-        await self._client.remote_control.skip_forward()
+    async def connection_lost(self, exception: Exception) -> None:
+        """Device was unexpectedly disconnected."""
+        System.out.println("Connection lost...")
+        new_connection = ScenarioAppleTV('192.168.10.246')
 
-    async def stop(self):
-        await self._client.remote_control.stop()
-
-    async def suspend(self):
-        await self._client.remote_control.suspend()
-
-    async def top_menu(self):
-        await self._client.remote_control.top_menu()
-
-    async def up(self):
-        await self._client.remote_control.up()
-
-    async def volume_down(self):
-        await self._client.remote_control.volume_down()
-
-    async def volume_up(self):
-        await self._client.remote_control.volume_up()
-
-    async def wakeup(self):
-        await self._client.remote_control.wakeup()
+        while not new_connection._client:
+            try:
+                await asyncio.wait_for(new_connection.connect(), 10)
+            except asyncio.TimeoutError:
+                System.out.println("Connection failed... trying again")
+            else:
+                self._client = new_connection._client
 
 
+class AppleTVListeners(PowerListener, PushListener, DeviceListener, ABC):
+    # PowerListener methods
+    def powerstate_update(
+            self, old_state: const.PowerState, new_state: const.PowerState
+    ):
+        System.out.println(str(new_state))
+
+    # PushListener methods
+    def playstatus_update(self, updater, playstatus: Playing) -> None:
+        if not playstatus:
+            self.connection_lost()
+        else:
+            System.out.println(" ")
+            System.out.println(" ")
+            System.out.println(str(playstatus))
+
+    def playstatus_error(self, updater, exception: Exception) -> None:
+        System.out.println(str(exception))
+
+    # DeviceListener methods
+    def connection_lost(self, exception: Exception) -> None:
+        """Device was unexpectedly disconnected."""
+        System.out.println("Connection lost...")
+
+    def connection_closed(self) -> None:
+        """Device connection was (intentionally) closed."""
+        System.out.println("Connection closed...")
--- src/main/java/com/example/appletvcontrol/Main.java
+++ src/main/java/com/example/appletvcontrol/Main.java
@@ -1,2 +1,48 @@
-package com.example.appletvcontrol;public class Main {
+
+package com.example.appletvcontrol;
+
+import jep.Jep;
+import jep.JepException;
+import jep.Interpreter;
+import jep.SharedInterpreter;
+
+public class Main {
+    public static void main(String[] args) throws JepException {
+        test_reconnection();
+    }
+
+    private static void test_commands() {
+        try (Interpreter interp = new SharedInterpreter();) {
+
+            String pyAppleTVPath = "./python/ScenarioAppleTV.py";
+            interp.runScript(pyAppleTVPath);
+            AppleTv atv = new AppleTv();
+            Listener listener = new Listener();
+            Publisher publisher = new Publisher(interp);
+            publisher.scan_network();
+            publisher.setListener(listener);
+            publisher.connect();
+            publisher.play_pause();
+            publisher.run_forever();
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void test_reconnection() {
+        try (Interpreter interp = new SharedInterpreter();) {
+            String pythonScriptFullPath = "./python/ScenarioAppleTV.py";
+            interp.runScript(pythonScriptFullPath);
+
+            interp.eval("apple_tv = ScenarioAppleTV('192.168.10.246')");
+            interp.eval("apple_tv._loop.run_until_complete(apple_tv.connect())");
+//        Thread.sleep(10000);
+//        interp.eval("await asyncio.gather(*apple_tv.close())");
+            interp.eval("apple_tv._loop.run_until_complete(apple_tv.check_if_alive())");
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
 }
